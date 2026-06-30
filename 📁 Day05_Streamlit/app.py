c2a9da857bae544affc91b8b8cab8f619743f87e
Muhammad Yaqoob Hassan <myaqoobh13@gmail.com>
2026-06-30 20:55:45 +0500

    Day05

--- 📁 Day05_Streamlit/app.py
+++ 📁 Day05_Streamlit/app.py
@@ -1,25 +1,28 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
-import numpy as np
+import plotly.express as px
+
+# For setting up title
 st.title("US airline sentimental analysis.")
+
+# A sidebar which displays about it
 st.sidebar.title("US airline sentimental analysis.")
 
+# Markdown
 st.markdown("This data is about the US airlines.")
 st.sidebar.markdown("This data is about the US airlines.")
 
-df = pd.read_csv("Tweets.csv")
 
 @st.cache_resource
-
-
 def load_data():
-    df['tweet_created'] = pd.to_datetime(df['tweet_created'])
-    return df
+    data = pd.read_csv("Tweets.csv")
+    data['tweet_created'] = pd.to_datetime(data['tweet_created'])
+    return data
 
 
 df = load_data()
-st.sidebar.subheader("Random Tweets.")
 
+st.sidebar.subheader("Random Tweets.")
 random_tweet = st.sidebar.radio("Sentimenal Analysis", ('positive', 'negative', 'neutral'))
 
 tweet = (
@@ -28,8 +31,40 @@ tweet = (
       .iloc[0]
 )
 
-
 st.write(df["airline_sentiment"].unique())
-
 st.sidebar.markdown(tweet)
 
+
+st.sidebar.markdown(" ### Number of tweets by sentiments.")
+select = st.sidebar.selectbox("Visualization", ("Histogram", "Pie chart"), key="viz_select")
+
+sentiments_count = df['airline_sentiment'].value_counts()
+sentiments_count = pd.DataFrame({'sentiment': sentiments_count.index, 'Tweets': sentiments_count.values})
+
+if not st.sidebar.checkbox("Hide", True, key="hide_checkbox"):
+    st.markdown(" ### Number of Tweets by sentiments.")
+    if select == 'Histogram':
+        fig = px.bar(sentiments_count, x='sentiment', y='Tweets', color='Tweets', height=500)
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(sentiments_count, values="Tweets", names='sentiment')
+        st.plotly_chart(fig)
+
+
+st.sidebar.markdown("When and where users are tweeting from?")
+
+hour = st.sidebar.slider("Hours of the day", 0, 23, key="hour_slider")
+
+if not st.sidebar.checkbox("close", True, key="close_checkbox"):
+    modified_data = df[df["tweet_created"].dt.hour == hour]
+    modified_data = modified_data.dropna(subset=['tweet_coord'])
+    modified_data[['latitude', 'longitude']] = (
+        modified_data['tweet_coord']
+        .str.strip('[]')
+        .str.split(',', expand=True)
+        .astype(float)
+    )
+
+    st.markdown("Tweets location based on the day.")
+    st.markdown("%i tweets between %i.00 and %i.00 " % (len(modified_data), hour, (hour + 1) % 24))
+    st.map(modified_data)
